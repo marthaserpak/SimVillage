e24327a8cd17ff12ddf0e525ee6c4bc0d5c77ffa
marthaserpak <m.v.serpak@gmail.com>
2020-07-02 18:27:36 +0300

    )

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,16 +1,15 @@
 fun main() {
-    /*val greetingFunction = { playerName: String, numBuildings: Int ->*/
-    runSimulation("Adler") { playerName, numBuildings ->
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2020
         println("Adding $numBuildings houses.")
         "Welcome to SimVillage, $playerName" +
                 " (copyright $currentYear)"
     }
 
-    /*runSimulation("Adler", greetingFunction)*/
+    runSimulation("Adler", greetingFunction)
 }
 
-fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
     val numBuildings = (1..9).shuffled().last() //случайно выберет 1,2...9
     println(greetingFunction(playerName,numBuildings))
 
